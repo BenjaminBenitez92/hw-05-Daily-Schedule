8c2d6765d32654cb31b3efb85a3c4bbf2205cf3b
BenjaminBenitez92 <81438491+BenjaminBenitez92@users.noreply.github.com>
2021-06-30 20:06:44 -0400

    almost last push

--- Script.js
+++ Script.js
@@ -1,11 +1,19 @@
 function currentTime() {
 var time = moment().format("dddd, MMM Do, hh:mm:ss");
 $("#currentDay" ).text(time);
-}
+};
 
-
-
-
+$(document).ready(function() {
+    // listen for save button clicks
+    $(".saveBtn").on("click", function() {
+      // get nearby values
+      var value = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+  
+      // save in localStorage
+      localStorage.setItem(time, value);
+    });
+});
 
 var currentHour = parseInt(moment().format('H'))
 $('.colorcode').each(function(){
@@ -19,6 +27,18 @@ $('.colorcode').each(function(){
     }else{
         $(this).css('background-color','gray');
     }
-    console.log(currentHour)
+      console.log(currentHour)
 });
+$("#hour9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+
+
  setInterval(currentTime, 1000);
